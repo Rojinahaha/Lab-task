2ae97891c4d797cc27dd51ff11e55f57b31ac75d
Rojinahaha <rojina.thapa@patancollege.edu.np>
2026-01-22 08:17:02 +0545

    Update lab1.sql

--- lab1.sql
+++ lab1.sql
@@ -1,4 +1,12 @@
-#Database Lab 1
-The repositary contains SQL lab work.
-##Contents
-- lab1.sql: Database creation and employee table
+Q1. Create a databse named dbesp and switch to it
+CREATE DATABASE dbesp;
+USE dbsesp;
+
+Q2. Create an employee table 
+CREATE TABLE employee(
+  emp_id INT PRIMARY KEY
+  AUTO_INCREMENT,
+  emp_name VARCHAR(100) NOT NULL,
+  email VARCHAR(100),
+  salary DECIMAL (10,2)
+  );
